4706c9aeaab8bb0cf57a08c8cd93c6a8384b8aa4
zenbox <michael@zenbox.de>
2020-02-10 11:28:03 +0100

    Update tools_corrected.js

--- assets/js/tools_corrected.js
+++ assets/js/tools_corrected.js
@@ -1,4 +1,5 @@
-/** Some node tools to use 
+/**
+ * 
  *
  * @package Webapplication
  * @module tools
@@ -11,70 +12,186 @@
 
 !(function () {
     'use strict';
-    // - - - - - - - - - -
-    // Declarations
-    // - - - - - - - - - -
 
-    // - - - - - - - - - -
-    // Functions
-    // - - - - - - - - - -
     /** function _getType()
      * 
-     * @param   {*}      t  _typeOfVariable 
-     * @returns {string|boolean} 
+     * @param {*} _typeOfVariable 
+     * @return {string} 
      */
-    function _getType(t) {
-        let _typeOfVariable = t || undefined;
-
-        // early exit
-        if (_typeOfVariable === undefined) return false;
-
+    function _getType(_typeOfVariable) {
         try {
             return Array.isArray(_typeOfVariable) ? 'array' : typeof (_typeOfVariable);
         } catch (_error) {
-            console.log('ERROR: function _getType' + _error);
-            return false;
+            console.log('ERROR: function tools::_getType' + _error);
         }
     }
 
     /** function _log
      * 
-     * @param   {string} n  _nameOfVariable
-     * @param   {*}      t  _typeOfVariable 
-     * @returns void
+     * @param {string} _name 
+     * @param {*} _typeOfVariable 
+     * @return void
      */
-    function _log(n, t) {
-        let
-            _nameOfVariable = n || undefined,
-            _typeOfVariable = t || undefined;
-
-        // early exits
-        if (_nameOfVariable === undefined) return false;
-        if (_typeOfVariable === undefined) return false;
-
+    function _log(_name, _typeOfVariable) {
         try {
-            console.log(_nameOfVariable, '{' + _getType(_typeOfVariable) + '}:', _typeOfVariable);
+            console.log(_name, '{' + _getType(_typeOfVariable) + '}:', _typeOfVariable);
         } catch (_error) {
-            console.log('ERROR: function _log: ' + _error);
+            console.log('ERROR: function tools::_log: ' + _error);
+        }
+    }
+
+    /** function _addNavigation
+     * 
+     * @param {object} _navigationData  
+     * @return void
+     */
+    function _addNavigation(_navigationData = []) {
+        let logInfo = "";
+        try {
+            if (_navigationData === null)
+                return;
+            logInfo = ' - Parameter 1: navigationData ' + '{' + _getType(_navigationData) + '}: ' + _navigationData.text;
+
+            let nav = document.createElement('nav');
+            let ul = nav.appendChild(document.createElement('ul'));
+
+            for (let i = 0; i < _navigationData.length; i++) {
+                let li = ul.appendChild(document.createElement('li'));
+                let a = li.appendChild(document.createElement('a'));
+                let text = document.createTextNode(_navigationData[i].text);
+                a.setAttribute('href', _navigationData[i].link);
+                a.addEventListener(eventType, callback); // Nachteil, es werden X Events etabliert
+                a.appendChild(text);
+            }
+            //            nav.addEventListener(eventType, callback);  // Nachteil -> event wird auch bei klick auf li, ul, nav + etc. ausgelöst
+            document.body.appendChild(nav); // Erst, wenn die Liste erzeugt ist wird sie verfügbar gemacht <- besserer Weg
+
+        } catch (_error) {
+            console.log('ERROR: function tools::_addNavigation: ' + _error + logInfo);
+        }
+    }
+
+
+    /**
+     * ! - - - - - - - - - - - - - - - -
+     * ! Das fängt mal mitten im Code an?
+     * ! Variablendeklarationen stehen besser am Anfang eines Scopes.
+     * ! Die Dokumentation der Funktion callback() ist unvollständig
+     * ! - - - - - - - - - - - - - - - - */
+
+    // declaration
+    let eventType = 'click';
+    let request = new XMLHttpRequest();
+
+    /** function callback()
+     * 
+     */
+    function callback(event) {
+        try {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+
+            /** 
+             * ! - - - - - - - - - - - - - - - -
+             * ! Das length ist unspezifisch, was möchtest du denn damit erreichen?
+             * ! Hier wäre ggf. ein erläuternder Kommentar für da Verständnis wichtig.
+             * ! - - - - - - - - - - - - - - - - */
+            if (event.target.attributes.length > 0)
+                loadContent(event.target.attributes[0].value, event.target.text);
+        } catch (_error) {
+            console.log('ERROR: function tools::callback(event)' + _error);
+        }
+    }
+
+    /** function onReadyStateChange
+     * 
+     * @return void
+     */
+
+    function onReadyStateChange() {
+        /**
+         * ! - - - - - - - - - - - - - - - -
+         * ! Ok.
+         * ! - - - - - - - - - - - - - - - - */
+        try {
+            switch (request.readyState) {
+                case 0:
+                    console.log('there is no request');
+                    break;
+                case 1:
+                    console.log('request opened');
+                    break;
+                case 2:
+                    console.log('request sent');
+                    break;
+                case 3:
+                    console.log('response first part ...');
+                    break;
+                case 4:
+                    console.log('response more parts and finished!');
+                    if (request.status === 200 || request.status === 304) {
+                        console.log(request.response);
+                        console.log('evaluation');
+
+                        /**
+                         * ! - - - - - - - - - - - - - - - -
+                         * ! Der Selektor "content" ist nicht generisch und sollte deshalb als Parameter übergeben werden.
+                         * ! - - - - - - - - - - - - - - - -
+                         */
+                        let ressource = document.getElementById('content');
+                        ressource.innerHTML = request.response;
+                    }
+                    break;
+                default:
+                    console.log('something strange happened!');
+                    break;
+            }
+        } catch (_error) {
+            console.log('ERROR: function tools::onReadyStateChange: ' + _error);
+        }
+    }
+
+
+    /** function loadContent
+     * 
+     * @param {string} url 
+     * @param {HTMLAnchorElement} context 
+     * @return void
+     */
+    function loadContent(url, context) {
+        /**
+         * ! - - - - - - - - - - - - - - - -
+         * ! Ein gordischer Knoten? 
+         * ! Der Eventlistener gehört in app.js in die main-Sektion, er nimmt ja Bezug auf das Dokument.
+         * ! tools.js ist rein generischer Code.
+         * ! loadContent() soll dann auch nur Content laden und nicht Events setzen 
+         * ! - - - - - - - - - - - - - - - - */
+        try {
+            request.addEventListener('readystatechange', onReadyStateChange);
+            request.open('GET', url);
+            request.send(context);
+        } catch (_error) {
+            console.log('ERROR: function tools::loadContent: ' + _error);
         }
     }
 
     /**
      * Calls the main procedure of scopes and types
-     * @returns void
+     * @return void
      */
     function _main() {
         try {
             window.tools = {} || window.tools;
             window.tools.log = _log;
+            window.tools.addNavigation = _addNavigation;
         } catch (_error) {
-            console.log('ERROR: function _main' + _error);
+            console.log('ERROR: function tools::_main' + _error);
         }
     }
 
 
     // - - - - - - - - - -
-    // Module Control
+    // Control
     // - - - - - - - - - -
     _main();
 })()
